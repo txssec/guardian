543eb0c683653e7c7a5619ea60c0115895b3ee04
João Lenon <lenonsec7@gmail.com>
2020-12-14 09:33:51 -0300

    feat: Owner middleware for resources

---
+++ app/Middleware/Owner.ts
@@ -0,0 +1,31 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import UnauthorizedException from 'App/Exceptions/UnauthorizedException'
+
+export default class Owner {
+  public async handle(ctx: HttpContextContract, next: () => Promise<void>) {
+    const user = ctx.auth.user
+
+    if (!user) {
+      throw new UnauthorizedException('Not found user in context')
+    }
+
+    await user.preload('roles')
+    const isAuthorized = user.roles.find((role) => role.slug === 'admin' || 'manager')
+
+    if (isAuthorized) {
+      await next()
+    }
+
+    if (ctx.request.method() === 'PUT' && ctx.params) {
+      const id = ctx.params.id
+
+      if (id !== user.id) {
+        throw new UnauthorizedException('User is not authorized to update resources of other user')
+      }
+
+      await next()
+    }
+
+    await next()
+  }
+}
