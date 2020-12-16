604fae35a72ca221b0e3561678d7622106e95cec
João Lenon <lenonsec7@gmail.com>
2020-12-16 12:39:34 -0300

    feat: appId and fromToken on seeders

--- database/seeders/UserSeeder.ts
+++ database/seeders/UserSeeder.ts
@@ -3,48 +3,26 @@ import { Role, User } from 'App/Models'
 
 export default class UserSeeder extends BaseSeeder {
   public async run() {
-    const userOne = await User.create({
+    await User.create({
       name: 'João Lenon',
       email: 'lenonsec7@gmail.com',
       password: '12345678',
       status: 'reproved',
+      applicationId: 'e4f1d0e5-f7db-4e1b-a1a0-244221e60c21',
+      fromToken: 'app-e4f1d0e5-f7db-4e1b-a1a0-244221e60c21',
     })
-
-    if (userOne) {
-      await userOne.related('attachments').create({
-        name: 'avatar',
-        type: 'avatar',
-        icon: 'avatar',
-        path: 'http://localhost:3333/123123.png',
-        size: '10mb',
-        extension: 'png',
-        from_token: userOne.token,
-        original_name: 'my-photo',
-      })
-    }
 
     const userTwo = await User.create({
       name: 'João Lenon',
       email: 'lenonsec7@hotmail.com',
       password: '12345678',
       status: 'approved',
+      applicationId: 'e4f1d0e5-f7db-4e1b-a1a0-244221e60c21',
+      fromToken: 'app-e4f1d0e5-f7db-4e1b-a1a0-244221e60c21',
     })
 
-    if (userTwo) {
-      await userTwo.related('attachments').create({
-        name: 'avatar',
-        type: 'avatar',
-        icon: 'avatar',
-        path: 'http://localhost:3333/123123.png',
-        size: '10mb',
-        extension: 'png',
-        from_token: userTwo.token,
-        original_name: 'my-photo',
-      })
+    const role = await Role.findByOrFail('slug', 'admin')
 
-      const role = await Role.findByOrFail('slug', 'admin')
-
-      await userTwo.related('roles').attach([role.id])
-    }
+    await userTwo.related('roles').attach([role.id])
   }
 }
