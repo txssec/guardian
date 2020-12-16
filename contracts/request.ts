d625f24f9a37544e2013c3478fa1f1b602a8ee33
João Lenon <lenonsec7@gmail.com>
2020-12-16 12:38:57 -0300

    feat: Add application to context contract

--- contracts/request.ts
+++ contracts/request.ts
@@ -4,5 +4,15 @@ declare module '@ioc:Adonis/Core/HttpContext' {
       page: number | string
       limit: number | string
     }
+    application: {
+      id: string
+      name: string
+      email: string
+      token: string
+      status: string
+      created_at: Date
+      update_at: Date
+      deleted_at: Date | null
+    }
   }
 }
