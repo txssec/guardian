6c899837e4164fb1d671954992aa64bef6a73bda
João Lenon <lenonsec7@gmail.com>
2020-12-16 12:35:33 -0300

    refactor: Extends from exception and handle

--- app/Exceptions/InternalServerException.ts
+++ app/Exceptions/InternalServerException.ts
@@ -1,7 +1,7 @@
-import BaseException from './BaseException'
+import { Exception } from '@poppinss/utils'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class InternalServerException extends BaseException {
+export default class InternalServerException extends Exception {
   constructor(
     message = 'An internal server error has ocurred, look for the logs to find what happened',
     status = 500,
@@ -10,7 +10,18 @@ export default class InternalServerException extends BaseException {
     super(message, status, code)
   }
 
-  public async handle(error: this, { response }: HttpContextContract) {
-    return this.response(error, response)
+  public async handle(error, ctx: HttpContextContract) {
+    return ctx.response.status(error.status).send({
+      status: error.status,
+      method: ctx.response.request.method,
+      code: error.code,
+      path: ctx.response.request.url,
+      timestamp: new Date().getTime(),
+      error: {
+        name: error.name,
+        help: 'BaseException',
+        message: error.message.split(': ')[1],
+      },
+    })
   }
 }
